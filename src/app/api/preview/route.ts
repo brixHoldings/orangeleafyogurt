89f0c32bf9b9add0ebf4f59f28d84e560cd55d0c
Rudi Aj <raj@dickeys.com>
2023-07-06 10:46:10 +0200

    fix: slices

--- src/app/api/preview/route.ts
+++ src/app/api/preview/route.ts
@@ -2,7 +2,7 @@ import { createClient } from 'prismicio';
 import { draftMode } from 'next/headers';
 import { redirectToPreviewURL } from '@prismicio/next';
 
-export async function GET(request) {
+export async function GET(request: any) {
   const client = createClient();
 
   draftMode().enable();
