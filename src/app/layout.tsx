59ac0eea78e1568cb084a80598af9847f568dcf3
Rudi Aj <raj@dickeys.com>
2023-07-06 12:02:44 +0200

    fix: add nav/footer on franchising

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,15 @@
 import { PrismicPreview } from '@prismicio/next';
 import localFont from 'next/font/local';
 import { repositoryName } from 'prismicio';
-import { useSelectedLayoutSegments } from 'next/navigation';
 
 import Footer from '@components/pages/OrangeLeaf/common/Footer';
 import Header from '@components/pages/OrangeLeaf/common/Header';
 import StyledComponentsRegistry from '@styles/registry';
 
-import type { FC, ReactNode } from 'react';
-import GlobalStyle from './orange-leaf/GlobalStyle';
 import { StoreProvider } from 'src/context/StoreProvider';
+import GlobalStyle from './orange-leaf/GlobalStyle';
+
+import type { FC, ReactNode } from 'react';
 
 const gothamUltra = localFont({
   src: '../../public/fonts/GothamUltra.otf',
@@ -24,26 +24,15 @@ const centuryGothic = localFont({
 });
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
-  const segments = useSelectedLayoutSegments();
-
-  const content =
-    segments[segments.length - 1] === 'franchising' ? (
-      children
-    ) : (
-      <>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </>
-    );
-
   return (
     <html className={`${gothamUltra.variable} ${centuryGothic.variable}`} lang="en">
       <body>
         <StoreProvider>
           <StyledComponentsRegistry>
             <GlobalStyle />
-            {content}
+            <Header />
+            <main>{children}</main>
+            <Footer />
             <PrismicPreview repositoryName={repositoryName} />
           </StyledComponentsRegistry>
         </StoreProvider>
