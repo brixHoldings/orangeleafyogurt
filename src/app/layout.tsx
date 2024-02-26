b77a19cd115250e2ec5f5cb7386fec933412da6a
Rudi Aj <raj@dickeys.com>
2024-02-26 16:10:12 +0100

    fix: add google tag script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,9 @@ const centuryGothic = localFont({
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <html className={`${gothamUltra.variable} ${centuryGothic.variable}`} lang="en">
+      <Script>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T37T4F8C')`}
+      </Script>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-H4J8CGKGZF"
         id="google-tag-manager"
@@ -42,6 +45,15 @@ const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
         `}
       </Script>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-
+T37T4F8C"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <StoreProvider>
           <StyledComponentsRegistry>
             <GlobalStyle />
