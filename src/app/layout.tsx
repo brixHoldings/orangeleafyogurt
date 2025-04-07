0a78917295852fb29724f57c7f1b8021af4229bc
raulsusanj <rsusanj@dickeys.com>
2025-04-07 13:30:28 +0200

    Moves accessability script to top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,12 @@ const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <html className={`${gothamUltra.variable} ${centuryGothic.variable}`} lang="en">
       <Script>
+        {`(function(){ var s = document.createElement(&#39;script&#39;); var h =
+          document.querySelector(&#39;head&#39;) || document.body; s.src =
+          &#39;https://acsbapp.com/apps/app/dist/js/app.js&#39;; s.async = true; s.onload =
+          function(){ acsbJS.init(); }; h.appendChild(s); })(); `}
+      </Script>
+      <Script>
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T37T4F8C')`}
       </Script>
       <Script
@@ -63,12 +69,6 @@ T37T4F8C"
             <PrismicPreview repositoryName={repositoryName} />
           </StyledComponentsRegistry>
         </StoreProvider>
-        <Script>
-          {`(function(){ var s = document.createElement(&#39;script&#39;); var h =
-          document.querySelector(&#39;head&#39;) || document.body; s.src =
-          &#39;https://acsbapp.com/apps/app/dist/js/app.js&#39;; s.async = true; s.onload =
-          function(){ acsbJS.init(); }; h.appendChild(s); })(); `}
-        </Script>
       </body>
     </html>
   );
