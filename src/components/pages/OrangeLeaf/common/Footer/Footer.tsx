90531feeaf2fe900ec7d5e1a700817c123088099
JohnHoward-Brix <157166380+JohnHoward-Brix@users.noreply.github.com>
2026-03-16 07:50:55 -0500

    Update Footer.tsx with 2026 Nutritional File link

--- src/components/pages/OrangeLeaf/common/Footer/Footer.tsx
+++ src/components/pages/OrangeLeaf/common/Footer/Footer.tsx
@@ -107,7 +107,7 @@ const Footer: FC = () => {
               <Separator />
               <Link href="/terms-of-use">Terms & Conditions</Link>
               <Separator />
-              <NativeLink download href="/files/Orange Leaf - Nutritional Chart 2025.pdf">
+              <NativeLink download href="/files/OrangeLeaf_NutritionalChart_2026.pdf">
                 Nutritional Info
               </NativeLink>
               <Separator />
