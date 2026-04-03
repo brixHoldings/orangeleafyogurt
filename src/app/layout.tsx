c2dd317243e56be5aea351d7ca971c48a848b0c7
JohnHoward-Brix <157166380+JohnHoward-Brix@users.noreply.github.com>
2026-04-03 10:07:23 -0500

    Update layout.tsx with cookie script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 'use client';
-
 import { PrismicPreview } from '@prismicio/next';
 import localFont from 'next/font/local';
 import { repositoryName } from 'prismicio';
-
 import Footer from '@components/pages/OrangeLeaf/common/Footer';
 import Header from '@components/pages/OrangeLeaf/common/Header';
 import StyledComponentsRegistry from '@styles/registry';
-
 import { StoreProvider } from 'src/context/StoreProvider';
 import GlobalStyle from './GlobalStyle';
-
 import type { FC, ReactNode } from 'react';
 import Script from 'next/script';
 
@@ -18,7 +14,6 @@ const gothamUltra = localFont({
   src: '../../public/fonts/GothamUltra.otf',
   variable: '--gotham-ultra',
 });
-
 const centuryGothic = localFont({
   src: '../../public/fonts/Gothic.ttf',
   variable: '--century-gothic',
@@ -27,6 +22,14 @@ const centuryGothic = localFont({
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <html className={`${gothamUltra.variable} ${centuryGothic.variable}`} lang="en">
+      {/* CookieYes - must load first before analytics scripts */}
+      <Script
+        id="cookieyes"
+        src="https://cdn-cookieyes.com/client_data/aaed38509f0052113bc94fa97a711cc6/script.js"
+        strategy="beforeInteractive"
+      />
+
+      {/* Google Tag Manager */}
       <Script>
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T37T4F8C')`}
       </Script>
@@ -34,21 +37,11 @@ const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
         src="https://www.googletagmanager.com/gtag/js?id=G-H4J8CGKGZF"
         id="google-tag-manager"
         strategy="afterInteractive"
-      >
-        {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag('js', new Date());
-      gtag('config', ''G-H4J8CGKGZF');
-        `}
-      </Script>
+      />
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-
-T37T4F8C"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T37T4F8C"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
@@ -65,22 +58,22 @@ T37T4F8C"
         </StoreProvider>
         <Script
           id="acsb-script"
-          strategy="lazyOnload" // Loads after everything else
+          strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `
-        (function() {
-          var s = document.createElement('script');
-          var h = document.querySelector('head') || document.body;
-          s.src = 'https://acsbapp.com/apps/app/dist/js/app.js';
-          s.async = true;
-          s.onload = function() {
-            if (typeof acsbJS !== 'undefined') {
-              acsbJS.init();
-            }
-          };
-          h.appendChild(s);
-        })();
-      `,
+              (function() {
+                var s = document.createElement('script');
+                var h = document.querySelector('head') || document.body;
+                s.src = 'https://acsbapp.com/apps/app/dist/js/app.js';
+                s.async = true;
+                s.onload = function() {
+                  if (typeof acsbJS !== 'undefined') {
+                    acsbJS.init();
+                  }
+                };
+                h.appendChild(s);
+              })();
+            `,
           }}
         />
       </body>
